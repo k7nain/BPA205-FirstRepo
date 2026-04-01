6731413ba53b00228c60e31158802e7c9ed95850
Kanan Mursalizada <murslizadkenan@gmail.com>
2026-04-01 17:37:07 +0400

    SchoolSystemModel (ReadOnly, Const, Static)

---
+++ SchoolSystemModel/SchoolSystemModel/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace SchoolSystemModel.Models
+{
+    public class Student
+    {
+        public static int Id { get; set; } = 1;
+        public string Name { get; set; }
+        public string  Surname { get; set; }
+        public int Age { get; set; }
+
+        public const string schoolName = "Code Academy";
+
+        public static int studentCount;
+
+        public readonly string studentCode;
+        public int studentId;
+
+        public Student(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            studentId = Id++;
+            studentCount++;
+            studentCode = new string(studentId.ToString()[0], 6);
+        }
+
+        public static void GetStatusCount()
+        {
+            Console.WriteLine($"Student count: {studentCount}");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id: {studentId}, Name: {Name}, Surname: {Surname}, Age: {Age}, StudentCode: {studentCode}, School Name: {schoolName} ");
+        }
+
+
+    }
+}
---
+++ SchoolSystemModel/SchoolSystemModel/Program.cs
@@ -0,0 +1,20 @@
+﻿using SchoolSystemModel.Models;
+
+namespace SchoolSystemModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Kanan", "Mursalizada", 21);
+            Student student2 = new Student("Omar", "Mirzeyev", 20);
+            Student student3 = new Student("Turan", "Allahverdiyev", 19);
+
+            student1.GetInfo();
+            student2.GetInfo();
+            student3.GetInfo();
+            Student.GetStatusCount();
+
+        }
+    }
+}
